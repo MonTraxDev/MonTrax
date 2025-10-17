414575f6d5546997f05e0cace85ca5dbc30f5b51
Ludovic Fournier <fouludovic@gmail.com>
2025-10-17 14:47:24 -0400

    prod

--- languages/lang.js
+++ languages/lang.js
@@ -2,11 +2,31 @@ const I18N = {
   fr: {
     "github.link1": "Lien vers le GitHub de l'API",
     "github.link2": "Lien vers le GitHub de cette page web",
-    "description": " MonTrax est un projet openSource qui a pour objectif d'afficher la postion en direct des train régional de montreal (EXO)."
+    description:
+      " MonTrax est un projet openSource qui a pour objectif d'afficher la postion en direct des train régional de montreal (EXO).",
+    "description.map": "MonTrax Carte",
+    "description.api": "MonTrax API",
+    about: "À propos",
+    "about.des":
+      "Comme mentionné plus tôt, MonTrax est un projet open source. Ce projet est lui-même séparé en trois projets distincts, chacun contenant une partie spécifique.",
+    project: "Les trois projet",
+    "project.map": "MonTrax Carte",
+    "project.api": "MonTrax API",
+    "project.board": "Montrax Planche",
   },
   en: {
     "github.link1": "Link to the API GitHub",
     "github.link2": "Link to the live Map website GitHub (This website)",
-    "description": "MonTrax is a openSource project ta provide live information and positon about Montréal regional train (EXO)."
+    description:
+      "MonTrax is a openSource project ta provide live information and positon about Montréal regional train (EXO).",
+    "montrax.map": "MonTrax Map",
+    "montrax.api": "MonTrax API",
+    about: "About",
+    "about.des":
+      "As mentioned earlier, MonTrax is an open-source project. It is itself divided into three distinct subprojects, each containing a specific component.",
+    project: "The three projects",
+    "project.map": "MonTrax Map",
+    "project.api": "MonTrax API",
+    "project.board": "Montrax Board",
   },
 };
